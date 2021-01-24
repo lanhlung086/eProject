ec5ada7e90cc0cf4d4c7e627bd58075f587fec40
DuyNguyen309 <71114562+DuyNguyen309@users.noreply.github.com>
2021-01-24 22:41:56 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import Product from "./Tung/Product";
 import Project from "./Quy/Project";
 import About from "./Quy/About";
 import ContactUs from "./Ngọc/Contactus";
+import ScrollToTop from "./Duy/ScrollToTop";
+import Detail from "./Khai/Detail";
+import SignUp from "./Duy/SignUp";
+import SignIn from "./Duy/SignIn";
+import Checkout from "./Duy/Checkout";
 
 function App() {
   return (
@@ -14,22 +19,53 @@ function App() {
           <Nav/>
           <Switch>
               <Route exact path="/">
+                  <ScrollToTop/>
+                  <Nav/>
                   <Home/>
               </Route>
-              <Route exact path="/product">
+              <Route exact path="/products">
+                  <ScrollToTop/>
+                  <Nav/>
                   <Product/>
               </Route>
+              <Route exact path="/productdetail/:id" render={(props) =>
+                  <>
+                      <ScrollToTop/>
+                      <Nav/>
+                      <Detail id={props.match.params.id}/>
+                  </>
+              }/>
               <Route exact path="/project">
-                  <Project></Project>
+                  <ScrollToTop/>
+                  <Nav/>
+                  <Project/>
               </Route>
               <Route exact path="/aboutus">
+                  <ScrollToTop/>
+                  <Nav/>
                   <About/>
               </Route>
               <Route exact path="/contact">
+                  <ScrollToTop/>
+                  <Nav/>
                   <ContactUs/>
               </Route>
               <Route exact path="/news">
-
+                  <ScrollToTop/>
+                  <Nav/>
+              </Route>
+              <Route exact path="/checkout">
+                  <ScrollToTop/>
+                  <Nav/>
+                  <Checkout/>
+              </Route>
+              <Route exact path="/signin">
+                  <ScrollToTop/>
+                  <SignIn/>
+              </Route>
+              <Route exact path="/signup">
+                  <ScrollToTop/>
+                  <SignUp/>
               </Route>
           </Switch>
       </div>
