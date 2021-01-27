5a1c9a47cabd3438e58aa41d9208cb1c5fbed2bc
quy2002 <trandinhquyzz2002@gmail.com>
2021-01-27 18:35:18 +0700

    up

--- src/Quy/About.js
+++ src/Quy/About.js
@@ -31,28 +31,32 @@ export default function About() {
                             <div className="mission row">
                                 <br/><img className="mission1 col-md-3"
                                           src="https://khaihoangroup.vn/wp-content/uploads/2019/07/su-menh.jpg"/>
-                                <p className="col-md-9"><u className="mission2">Mission</u><br/> <b>Fan
-                                    Group</b> mission is to bring high quality products and services, for a cool life
-                                    for the entire community.</p><br/>
+                                <p className="col-md-9"><u className="mission2">Mission</u><br/> <b>- Fan
+                                    Group</b> always set the goal of sustainable development, must be attached to preserving the environment and actively contributing to the overall development of the whole society, bringing the Vietnamese brand to the world.<br/><br/>
+                                    - Train and develop human resources into a professional workforce, high labor productivity, always with the spirit of innovation, solidarity and happiness.<br/>
+                                </p><br/>
                             </div>
                             <br/>
                             <div className="orientation row">
                                 <br/>
-                                <p className="col-md-9"><u className="orientation2">Orientation</u><br/><b>Fan
-                                    Group</b> gradually realizes its goal of becoming a leading corporation in Vietnam
-                                    with the desire to create a breakthrough for the domestic and regional economy.</p>
+                                <p className="col-md-9"><u className="orientation2">Orientation</u><br/><b>- Fan
+                                    Group</b> is gradually realizes its goal of becoming a leading corporation in Vietnam
+                                    with the desire to create a breakthrough for the domestic and regional economy.<br/><br/>
+                                    - Build a team of leaders with bravery and professionalism, at the same time build a team of managers, businessmen, good experts.
+                                </p>
                                 <img className="orientation1 col-md-3"
-                                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8w6cCTsdHrpFqlksTvK3dV_Jo4aDDyV8mvQ&usqp=CAU"/>
+                                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8w6cCTsdHrpFqlksTvK3dV_Jo4aDDyV8mvQ&usqp=CAU   "/>
                                 <br/>
                             </div>
                             <br/>
                             <div className="capacity row">
                                 <br/><img className="capacity1 col-md-3"
                                           src="https://khaihoangroup.vn/wp-content/uploads/2019/07/nang-luc-2.jpg"/>
-                                <p className="col-md-9"><u className="capacity2">Capacity</u><br/>With the right
+                                <p className="col-md-9"><u className="capacity2">Capacity</u><br/>- With the right
                                     investment and development strategy, sustainable financial resources, qualified
                                     personnel and modern facilities, effective application of information
-                                    technology, <b>Fan Group</b> system is continuing to be broadcast. developing
+                                    technology.<br/><br/>
+                                    <b>- Fan Group</b> system is continuing to be broadcast. developing
                                     strongly and consistently, ready to meet the Group's further expansion needs in the
                                     future.</p>
                             </div>
